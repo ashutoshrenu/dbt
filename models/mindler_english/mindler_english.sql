f0ec19edbe0cba63b41ef9197cee7ee41e3d63cb
Ashutosh Renu <ashutoshrenu@ip-192-168-211-57.ap-south-1.compute.internal>
2021-10-06 00:38:38 +0530

    update and delete statement

--- models/mindler_english/mindler_english.sql
+++ models/mindler_english/mindler_english.sql
@@ -11,5 +11,9 @@ final as (
 select * from final
 
 {{ config(
+   post_hook="UPDATE cart_page_leads SET name='Vishal' where email='vishal.yadav@mindler.com' | DELETE FROM cart_page_leads WHERE email='surenderonav@gmail.com'"
+) }}
+
+{{ config(
    post_hook="DELETE FROM cart_page_leads WHERE email='surenderonav@gmail.com'"
 ) }}
